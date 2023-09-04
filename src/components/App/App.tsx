be7d76a198e45975ad14c368e4409b50817e1c73
davidpinkevich <davidpinkevich95@gmail.com>
2023-09-04 23:41:18 +0300

    feat: add logic for app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,54 @@
-import "./App.css";
+import { useState } from "react";
+import { Collapse, Stack, Title } from "@mantine/core";
+import { InputField } from "../InputField/InputField";
+import { Items } from "../Items/Items";
+import { Filter } from "../Filter/Filter";
+import { useItems } from "../../hooks/useItems";
+import { useInput } from "../../hooks/useInput";
+import { useFilter } from "../../hooks/useFilter";
+import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  const [opened, setOpened] = useState<boolean>(true);
+  const { input, setInput, addText } = useInput();
+  const { items, changeItems, addItem, changeStatus, clearCompleted } =
+    useItems(input, setInput);
+  const { filter, changefilter } = useFilter();
+
+  return (
+    <div className="App">
+      <Title className="main-title" order={1}>
+        ToDo list:
+      </Title>
+      <Stack className="container" maw={800} mx="auto" spacing="14px">
+        <InputField
+          opened={opened}
+          setOpened={(arg) => setOpened(!arg)}
+          addItem={addItem}
+          addText={addText}
+          input={input}
+        />
+        <Collapse
+          in={opened}
+          transitionDuration={600}
+          transitionTimingFunction="linear"
+        >
+          <Items
+            filter={filter}
+            items={items}
+            changeStatus={changeStatus}
+            changeItems={changeItems}
+          />
+        </Collapse>
+        <Filter
+          items={items}
+          filter={filter}
+          changefilter={changefilter}
+          clearCompleted={clearCompleted}
+        />
+      </Stack>
+    </div>
+  );
 }
 
 export default App;
