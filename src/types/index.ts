69f8948795589e627ce9497f2ba5bd65d169085d
davidpinkevich <davidpinkevich95@gmail.com>
2023-09-04 23:33:54 +0300

    feat: add types

---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+import { ChangeEvent } from "react";
+
+export interface ITodoItem {
+  text: string;
+  id: string;
+  completed: string;
+}
+
+export interface ItemProps {
+  text: string;
+  id: string;
+  completed: string;
+  changeStatus: (id: string) => void;
+}
+
+export interface ItemsProps {
+  items: ITodoItem[];
+  filter: string;
+  changeStatus: (id: string) => void;
+  changeItems: (items: ITodoItem[]) => void;
+}
+
+export interface InputFieldPros {
+  opened: boolean;
+  input: string;
+  setOpened: (arg: boolean) => void;
+  addItem: (text: string) => void;
+  addText: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface IFilter {
+  filter: string;
+  items: ITodoItem[];
+  changefilter: (filter: string) => void;
+  clearCompleted: () => void;
+}
